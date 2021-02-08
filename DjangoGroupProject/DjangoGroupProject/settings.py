2fe9aa0cd98388d1b9216b98aa913afc45baa134
moonks90 <moonks90@gmail.com>
2021-02-08 14:17:46 -0500

    Initial commit

--- DjangoGroupProject/DjangoGroupProject/settings.py
+++ DjangoGroupProject/DjangoGroupProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'g8r-f&-vtbd52bh$po@1vdpy$4q0&k^h%=4!(=xpq)3b4itbl$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
