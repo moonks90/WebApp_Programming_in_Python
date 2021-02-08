918854d577806b6f637c4c04ac9d78c5b33c3353
moonks90 <moonks90@gmail.com>
2021-02-08 14:03:51 -0500

    Testing webpage
    Module 4

--- DjangoGroupProject/DjangoGroupProject/urls.py
+++ DjangoGroupProject/DjangoGroupProject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from applications import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='homepage'),
+    path('testpage', views.testpage, name='testpage'),
+    path('printnamepage', views.printnamepage, name='printnamepage'),
 ]
--- DjangoGroupProject/applications/views.py
+++ DjangoGroupProject/applications/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    data = dict()
+    return render(request,"home.html",context=data)
+
+
+def testpage(request):
+    data = dict()
+    return render(request,"testpage.html",context=data)
+
+
+def printnamepage(request):
+    data = dict()
+    user = request.GET['name']
+    data['name'] = user
+    return render(request,"printname.html",context=data)
